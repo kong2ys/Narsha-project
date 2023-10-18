e9772a32a8fc1acb3250f8d99eed41b5a9d2219f
kong2ys <yousung0525@naver.com>
2023-10-18 23:54:42 +0900

    Add GameManager, Spawner(Enemy) etc...
    GameManager 추가
    Spawner 추가(Enemy용)
    PoolManager 완료

--- Assets/Scripts/Manager/PoolManager.cs
+++ Assets/Scripts/Manager/PoolManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,37 @@ public class PoolManager : MonoBehaviour
     public GameObject[] prefabs;
 
     private List<GameObject>[] pools;
+
+    private void Awake()
+    {
+        pools = new List<GameObject>[prefabs.Length];
+
+        for (int index = 0; index < pools.Length; index++)
+        {
+            pools[index] = new List<GameObject>();
+        }
+    }
     
-    
+    public GameObject Get(int index)
+    {
+        GameObject select = null;
+
+        foreach (GameObject item in pools[index])
+        {
+            if (item.activeSelf)
+            {
+                select = item;
+                select.SetActive(true);
+                break;
+            }
+        }
+
+        if (!select)
+        {
+            select = Instantiate(prefabs[index], transform);
+            pools[index].Add(select);
+        }
+
+        return select;
+    }
 }
