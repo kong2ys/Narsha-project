64f55f58e2b6daaf4637c7e4a4d333053d7a7078
Yoimiya <kgw4589@gmail.com>
2023-10-24 20:46:06 +0900

    10.24 액티브 스킬 UI 임시로 만듦
    5렙에 액티브 스킬 1번 활성화

---
+++ Assets/Scripts/UGUI/SkillUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using Image = UnityEngine.UI.Image;
+
+public class SkillUI : MonoBehaviour
+{
+    public Sprite[] skillIcon;
+    public Image[] activeSkillUI;
+
+    void Update()
+    {
+        if (GameDataManager.Instance.TurretLevel >= 1)
+        {
+            activeSkillUI[0].sprite = skillIcon[0];
+        }
+    }
+}
