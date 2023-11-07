487af133a1a67f2bfc63db8ed0daf523068de4d3
Yoimiya <kgw4589@gmail.com>
2023-11-07 18:24:55 +0900

    Add Player ExpBar

---
+++ Assets/Scripts/UGUI/HUD.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public enum InfoType { Exp, Level, Kill, Time, Health }
+    public InfoType type;
+
+    Text _mytext;
+    Slider _mySlider;
+
+    void Awake()
+    {
+        _mytext = GetComponent<Text>();
+        _mySlider = GetComponent<Slider>();
+    }
+
+    void LateUpdate()
+    {
+        switch (type)
+        {
+            case InfoType.Exp:
+            {
+                float currentExp = GameDataManager.Instance.CurrentExp;
+                float maxExp = GameDataManager.Instance.MaxExp;
+                _mySlider.value = currentExp / maxExp;
+                break;
+            }
+            case InfoType.Level:
+            {
+                
+                break;
+            }
+            case InfoType.Kill:
+            {
+                
+                break;
+            }
+            case InfoType.Time:
+            {
+                
+                break;
+            }
+            case InfoType.Health:
+            {
+                
+                break;
+            }
+            
+        }
+    }
+}
