ce03a46a94ce2215fe1ad68960a0568f8195f51b
dreamer603 <mj617066@gmail.com>
2023-12-05 16:50:41 +0900

    BossScene Wall Prefab Change And MapManager Edit

--- Assets/Scripts/Boss/Balltan.cs
+++ Assets/Scripts/Boss/Balltan.cs
@@ -32,6 +32,9 @@ public class Balltan : MonoBehaviour
     private bool drawLine = false;
     private bool attack = true;
 
+    private float _duration = 1f;
+    private float _targetY = 0;
+    
     private Vector3 _dir;
     
     public Transform targetPos;    // 부채꼴에 포함되는지 판별할 타겟
@@ -134,7 +137,7 @@ public class Balltan : MonoBehaviour
                     isAlive = true;
                 }
                 var position = lookTarget.transform.position;
-                transform.position = new Vector3(position.x + 5,position.y,position.z);
+                transform.position = new Vector3(position.x + 5, position.y, position.z);
                 _move = false;
                 Wall();
                 break;
@@ -186,12 +189,12 @@ public class Balltan : MonoBehaviour
     }
     void Wall()
     {
- 
         currentTime -= Time.deltaTime;
         if (currentTime < 0)
         {
-            _circle.transform.position = transform.position;
-            _circle.SetActive(true); 
+            _circle.transform.position = new Vector3(transform.position.x, -100, transform.position.z);
+            _circle.SetActive(true);
+            StartCoroutine(MoveFence(_circle));
             summonWall = true; 
             pattern = 0; 
             currentTime = 0;
@@ -305,5 +308,24 @@ public class Balltan : MonoBehaviour
                     
             }
         }
+    }
+    
+    IEnumerator MoveFence(GameObject fence)
+    {
+        Vector3 startPosition = fence.transform.position;
+        float elapsed = 0f;
+
+        while (elapsed < _duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / _duration;
+            Vector3 newPosition = new Vector3(fence.transform.position.x,
+                Mathf.LerpUnclamped(startPosition.y, _targetY, t), fence.transform.position.z);
+            fence.transform.position = newPosition;
+            yield return null;
+        }
+
+        Vector3 finalPosition = new Vector3(fence.transform.position.x, _targetY, fence.transform.position.z);
+        fence.transform.position = finalPosition;
     }
 }
--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -7,24 +7,24 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    public GameObject[] mappingObj;     // 9개 타일 오브젝트
-    public GameObject fence;            // 발탄 보스전 구조물 오브젝트
-    public GameObject castle;           // boss2 보스전 구조물 오브젝트
-    public GameObject portal;           // portal 구조물 오브젝트
-    public GameObject player;           // player 오브젝트
-    public GameObject balltan;          // 발탄 오브젝트
+    public GameObject[] mappingObj; // 9개 타일 오브젝트
+    public GameObject[] bossObj;    // 보스 구조물 오브젝트 ([0]: fence / [1]: castle / [2]: portal)
+    public GameObject player; // player 오브젝트
+
+    public GameObject balltan;      // 발탄 오브젝트
     // public GameObject boss2;            // boss2 오브젝트
-    public GameObject poolManager;
-    public float tileSize;              // 타일 1개 길이
+    public GameObject poolManager;  // poolManager 오브젝트
+    
+    public float tileSize; // 타일 1개 길이
 
-    private float _dirFront;     // 앞 타일 거리
-    private float _dirBack;      // 뒤 타일 거리
-    private float _dirRight;     // 오른쪽 타일 거리  
-    private float _dirLeft;      // 왼쪽 타일 거리
+    private float _dirFront; // 앞 타일 거리
+    private float _dirBack;  // 뒤 타일 거리
+    private float _dirRight; // 오른쪽 타일 거리  
+    private float _dirLeft;  // 왼쪽 타일 거리
 
-    private float _posX;         // 플레이어 오브젝트 x값 위치
-    private float _posZ;         // 플레이어 오브젝트 z값 위치
-    private float _movePos;      // 타일 이동값
+    private float _posX;    // 플레이어 오브젝트 x값 위치
+    private float _posZ;    // 플레이어 오브젝트 z값 위치
+    private float _movePos; // 타일 이동값
 
     private float _duration = 1f;       // 구조물 등장 소요 시간
     private float _reDuration = 5f;     // 구조물 사라짐 소요 시간
@@ -32,17 +32,32 @@ public class MapManager : MonoBehaviour
     private float _castleTargetY = 20;  // castle 구조물 올라오는 위치
     private float _reTargetY = -100;    // 구조물 내려가는 위치
 
-    private bool _isBalltanLive = false;         // 발탄 생존 여부 플래그
-    private bool _isBoss2Live;           // boss2 생존 여부 플래그
+    private bool _isBalltanLive = false; // 발탄 생존 여부 플래그
+    private bool _isBoss2Live = false;   // boss2 생존 여부 플래그
 
-    private Balltan _balltanScript;      // 발탄 스크립트 참조
-    // private Boss2 _boss2Script;          // boss2 스크립트 참조
-    private GameDataManager _gmScript;
+    private Balltan _balltanScript;    // 발탄 스크립트 참조
+    // private Boss2 _boss2Script;     // boss2 스크립트 참조
+    private GameDataManager _gmScript; // 게임 데이터 매니저 참조
 
-    private bool _isFence = false;       // fance 등장 상태 플래그
-    private bool _isCastle = false;      // castle 등장 상태 플래그
-    private bool _isPortal = false;      // portal 등장 상태 플래그
-    
+    private bool _isFence = false;  // fence 등장 상태 플래그
+    private bool _isCastle = false; // castle 등장 상태 플래그
+    private bool _isPortal = false; // portal 등장 상태 플래그
+
+    private int[] _frontSwap;
+    private int[] _backSwap;
+    private int[] _rightSwap;
+    private int[] _leftSwap;
+
+    private int[] _frontMov;
+    private int[] _backMov;
+    private int[] _rightMov;
+    private int[] _leftMov;
+
+    private float[] _frontPos;
+    private float[] _backPos;
+    private float[] _rightPos;
+    private float[] _leftPos;
+
     private void Start()
     {
         _dirFront = tileSize / 2;
@@ -50,12 +65,27 @@ public class MapManager : MonoBehaviour
         _dirRight = tileSize / 2;
         _dirLeft = tileSize / 2 * -1;
 
-        _movePos = tileSize * 3;     // 3 * 3 타일 => 타일 세 칸 넓이 이동
-        
+        _movePos = tileSize * 3; // 3 * 3 타일 => 타일 세 칸 넓이 이동
+
+        _frontSwap = new int[] { 6, 3, 0, 7, 4, 1, 8, 5, 2 };
+        _backSwap = new int[] { 0, 3, 6, 1, 4, 7, 2, 5, 8 };
+        _rightSwap = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+        _leftSwap = new int[] { 2, 1, 0, 5, 4, 3, 8, 7, 6 };
+
+        _frontMov = new int[] { 0, 1, 2 };
+        _backMov = new int[] { 6, 7, 8 };
+        _rightMov = new int[] { 2, 5, 8 };
+        _leftMov = new int[] { 0, 3, 6 };
+
+        _frontPos = new float[] { 0, 0, _movePos, 0, 0, tileSize };
+        _backPos = new float[] { 0, 0, -_movePos, 0, 0, -tileSize };
+        _rightPos = new float[] { _movePos, 0, 0, tileSize, 0, 0 };
+        _leftPos = new float[] { -_movePos, 0, 0, -tileSize, 0, 0 };
+
         // Balltan _balltanScript = balltan.GetComponent<Balltan>();
         // Boss2 _boss2Script = boss2.GetComponent<Boss2>();
     }
-    
+
     void Update()
     {
         // 보스 생존 플래그 설정
@@ -64,143 +94,101 @@ public class MapManager : MonoBehaviour
 
         if (GameDataManager.Instance.KillScore >= 300 || Input.GetKeyDown(KeyCode.G))
         {
-            Debug.Log("100");
-            StartCoroutine(MoveFence(fence));
-            StartCoroutine(MovePortal(portal));
+            StartCoroutine(MoveFence(bossObj[0]));
+            StartCoroutine(MoveUnder(bossObj[2], _targetY));
             _isBalltanLive = true;
             poolManager.SetActive(false);
         }
-        
+
         if (_isBalltanLive && !_isFence)
         {
-            StartCoroutine(MoveFence(fence));
+            StartCoroutine(MoveFence(bossObj[0]));
             _isFence = true;
         }
         else if (!_isBalltanLive && _isFence)
         {
-            StartCoroutine(ReMoveFence(fence));
+            StartCoroutine(RemoveObj(bossObj[0]));
             _isFence = false;
         }
 
         if (_isBoss2Live && !_isCastle)
         {
-            StartCoroutine(MoveCastle(castle));
+            StartCoroutine(MoveUnder(bossObj[1], _castleTargetY));
             _isCastle = true;
         }
         else if (!_isBoss2Live && _isCastle)
         {
-            StartCoroutine(ReMoveFence(castle));
+            StartCoroutine(RemoveObj(bossObj[1]));
             _isCastle = false;
         }
-        
+
         if (_isPortal)
         {
-            StartCoroutine(MovePortal(portal));
+            StartCoroutine(MoveUnder(bossObj[2], _castleTargetY));
             _isPortal = false;
         }
-        
+
         // 보스 생존 시 맵 이동 제한
-        if (_isBalltanLive||_isBoss2Live)
+        if (_isBalltanLive || _isBoss2Live)
         {
-             return;
+            return;
         }
-        
+
         _posX = player.gameObject.transform.position.x;
         _posZ = player.gameObject.transform.position.z;
-        
+
         // 지정해둔 타일 거리보다 더 이동했으면 타일 이동 후 위치에 맞춰서 거리값 변경
-        
+
         if (_posZ >= _dirFront)
         {
-            FrontMoveMap();
+            MoveMap(_frontSwap, _frontMov, _frontPos);
             _dirFront += tileSize;
             _dirBack += tileSize;
         }
 
         if (_posZ <= _dirBack)
         {
-            BackMoveMap();
+            MoveMap(_backSwap, _backMov, _backPos);
             _dirFront -= tileSize;
             _dirBack -= tileSize;
         }
 
         if (_posX >= _dirRight)
         {
-            RightMoveMap();
+            MoveMap(_rightSwap, _rightMov, _rightPos);
             _dirRight += tileSize;
             _dirLeft += tileSize;
         }
 
         if (_posX <= _dirLeft)
         {
-            LeftMoveMap();
+            MoveMap(_leftSwap, _leftMov, _leftPos);
             _dirRight -= tileSize;
             _dirLeft -= tileSize;
         }
     }
-    
-    void FrontMoveMap()
+
+    void MoveMap(int[] swapList, int[] objList, float[] posList)
     {
-        Swap(mappingObj, 6, 3, 0);
-        Swap(mappingObj, 7, 4, 1);
-        Swap(mappingObj, 8, 5, 2);
-        mappingObj[0].transform.position += new Vector3(0, 0, _movePos);
-        mappingObj[1].transform.position += new Vector3(0, 0, _movePos);
-        mappingObj[2].transform.position += new Vector3(0, 0, _movePos);
-        fence.transform.position += new Vector3(0, 0, tileSize);
-        castle.transform.position += new Vector3(0, 0, tileSize);
-        portal.transform.position += new Vector3(0, 0, tileSize);
-    }
-    
-    void BackMoveMap()
-    {
-        Swap(mappingObj, 0, 3, 6);
-        Swap(mappingObj, 1, 4, 7);
-        Swap(mappingObj, 2, 5, 8);
-        mappingObj[6].transform.position += new Vector3(0, 0, -_movePos);
-        mappingObj[7].transform.position += new Vector3(0, 0, -_movePos);
-        mappingObj[8].transform.position += new Vector3(0, 0, -_movePos);
-        fence.transform.position += new Vector3(0, 0, -tileSize);
-        castle.transform.position += new Vector3(0, 0, -tileSize);
-        portal.transform.position += new Vector3(0, 0, -tileSize);
+        Swap(mappingObj, swapList);
+        for (int i = 0; i < 3; i++)
+        {
+            mappingObj[objList[i]].transform.position += new Vector3(posList[0], posList[1], posList[2]);
+            bossObj[i].transform.position += new Vector3(posList[3], posList[4], posList[5]);
+        }
     }
 
-    void RightMoveMap()
+    void Swap(GameObject[] mapTile, int[] sl)
     {
-        Swap(mappingObj, 0, 1, 2);
-        Swap(mappingObj, 3, 4, 5);
-        Swap(mappingObj, 6, 7, 8);
-        mappingObj[2].transform.position += new Vector3(_movePos, 0, 0);
-        mappingObj[5].transform.position += new Vector3(_movePos, 0, 0);
-        mappingObj[8].transform.position += new Vector3(_movePos, 0, 0);
-        fence.transform.position += new Vector3(tileSize, 0, 0);
-        castle.transform.position += new Vector3(tileSize, 0, 0);
-        portal.transform.position += new Vector3(tileSize, 0, 0);
+        for (int i = 0; i < 7; i = i + 3)
+        {
+            GameObject dummy = mapTile[sl[i]];
+            mapTile[sl[i]] = mapTile[sl[i + 1]];
+            mapTile[sl[i + 1]] = mapTile[sl[i + 2]];
+            mapTile[sl[i + 2]] = dummy;
+        }
     }
 
-    void LeftMoveMap()
-    {
-        Swap(mappingObj, 2, 1, 0);
-        Swap(mappingObj, 5, 4, 3);
-        Swap(mappingObj, 8, 7, 6);
-        mappingObj[0].transform.position += new Vector3(-_movePos, 0, 0);
-        mappingObj[3].transform.position += new Vector3(-_movePos, 0, 0);
-        mappingObj[6].transform.position += new Vector3(-_movePos, 0, 0);
-        fence.transform.position += new Vector3(-tileSize, 0, 0);
-        castle.transform.position += new Vector3(-tileSize, 0, 0);
-        portal.transform.position += new Vector3(-tileSize, 0, 0);
-    }
-    
-    void Swap(GameObject[] mapTile, int i, int j, int k)
-    {
-        // ex) 오른쪽 이동 시 맨 윗줄 타일이 [0][1][2] -> [1][2][0]으로 변경되는 방식
-        
-        GameObject test = mapTile[i];
-        mapTile[i] = mapTile[j];
-        mapTile[j] = mapTile[k];
-        mapTile[k] = test;
-    }
-    
     IEnumerator MoveFence(GameObject fence)
     {
         Vector3 startPosition = fence.transform.position;
@@ -219,82 +207,42 @@ public class MapManager : MonoBehaviour
         Vector3 finalPosition = new Vector3(fence.transform.position.x, _targetY, fence.transform.position.z);
         fence.transform.position = finalPosition;
     }
-    
-    IEnumerator ReMoveFence(GameObject fence)
+
+    IEnumerator RemoveObj(GameObject obj)
     {
-        Vector3 startPosition = fence.transform.position;
+        Vector3 startPosition = obj.transform.position;
         float elapsed = 0f;
 
         while (elapsed < _reDuration)
         {
             elapsed += Time.deltaTime;
             float t = elapsed / _duration;
-            Vector3 newPosition = new Vector3(fence.transform.position.x,
-                Mathf.LerpUnclamped(startPosition.y, _reTargetY, t), fence.transform.position.z);
-            fence.transform.position = newPosition;
+            Vector3 newPosition = new Vector3(obj.transform.position.x,
+                Mathf.LerpUnclamped(startPosition.y, _reTargetY, t), obj.transform.position.z);
+            obj.transform.position = newPosition;
             yield return null;
         }
 
-        Vector3 finalPosition = new Vector3(fence.transform.position.x, _reTargetY, fence.transform.position.z);
-        fence.transform.position = finalPosition;
+        Vector3 finalPosition = new Vector3(obj.transform.position.x, _reTargetY, obj.transform.position.z);
+        obj.transform.position = finalPosition;
     }
 
-    IEnumerator MoveCastle(GameObject castle)
+    IEnumerator MoveUnder(GameObject under, float underTargetY)
     {
-        Vector3 startPosition = castle.transform.position;
+        Vector3 startPosition = under.transform.position;
         float elapsed = 0f;
 
         while (elapsed < _duration)
         {
             elapsed += Time.deltaTime;
             float t = elapsed / _duration;
-            Vector3 newPosition = new Vector3(castle.transform.position.x,
-                Mathf.LerpUnclamped(startPosition.y, _castleTargetY, t), castle.transform.position.z);
-            castle.transform.position = newPosition;
+            Vector3 newPosition = new Vector3(under.transform.position.x,
+                Mathf.LerpUnclamped(startPosition.y, underTargetY, t), under.transform.position.z);
+            under.transform.position = newPosition;
             yield return null;
         }
 
-        Vector3 finalPosition = new Vector3(castle.transform.position.x, _castleTargetY, castle.transform.position.z);
-        castle.transform.position = finalPosition;
+        Vector3 finalPosition = new Vector3(under.transform.position.x, underTargetY, under.transform.position.z);
+        under.transform.position = finalPosition;
     }
-    
-    IEnumerator ReMoveCastle(GameObject castle)
-    {
-        Vector3 startPosition = castle.transform.position;
-        float elapsed = 0f;
-
-        while (elapsed < _reDuration)
-        {
-            elapsed += Time.deltaTime;
-            float t = elapsed / _duration;
-            Vector3 newPosition = new Vector3(castle.transform.position.x,
-                Mathf.LerpUnclamped(startPosition.y, _reTargetY, t), castle.transform.position.z);
-            castle.transform.position = newPosition;
-            yield return null;
-        }
-
-        Vector3 finalPosition = new Vector3(castle.transform.position.x, _reTargetY, castle.transform.position.z);
-        castle.transform.position = finalPosition;
-    }
-    
-    IEnumerator MovePortal(GameObject portal)
-    {
-        Vector3 startPosition = portal.transform.position;
-        float elapsed = 0f;
-
-        while (elapsed < _duration)
-        {
-            elapsed += Time.deltaTime;
-            float t = elapsed / _duration;
-            Vector3 newPosition = new Vector3(portal.transform.position.x,
-                Mathf.LerpUnclamped(startPosition.y, _targetY, t), portal.transform.position.z);
-            portal.transform.position = newPosition;
-            yield return null;
-        }
-
-        Vector3 finalPosition = new Vector3(portal.transform.position.x, _targetY, portal.transform.position.z);
-        portal.transform.position = finalPosition;
-    }
-    
 }
-
